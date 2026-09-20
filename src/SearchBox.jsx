0e38b3a2e735b4fd5f06f1edd9d9de0b3f1d2944
Tejashwini Singwal <tejashwinisingwal@gmail.com>
2026-09-20 23:34:51 +0530

    Update SearchBox.jsx

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -7,7 +7,7 @@ export default function SearchBox({updateInfo}) {
     let [city,setCity]= useState("");
     let [error,setError]= useState(false);
     const API_URL ="https://api.openweathermap.org/data/2.5/weather";
-    const API_KEY= "5468f16d7b70d61a2af37cffe7889e34";
+    const API_KEY= ".";
 
 
     let getWeatherInfo = async () =>{
